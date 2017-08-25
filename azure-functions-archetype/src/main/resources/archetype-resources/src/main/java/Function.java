9d6fe8541f54da2a35382f8b3cad161b16e236f5
Kevin Zhao <kevinzha@microsoft.com>
2017-08-25 17:00:10 +0800

    add archetype for functions

---
+++ azure-functions-archetype/src/main/resources/archetype-resources/src/main/java/Function.java
@@ -0,0 +1,16 @@
+package $package;
+
+import com.microsoft.azure.serverless.functions.annotation.*;
+import com.microsoft.azure.serverless.functions.ExecutionContext;
+
+/**
+ * Hello function with HTTP Trigger.
+ *
+ */
+public class Function {
+    @FunctionName("hello")
+    public String hello(@HttpTrigger(name = "req", authLevel = AuthorizationLevel.ANONYMOUS) String req,
+                        ExecutionContext context) {
+        return String.format("Hello, %s!", req);
+    }
+}
